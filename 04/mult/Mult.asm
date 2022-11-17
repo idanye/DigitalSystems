9606d651613c511a90db9d80a6ab8a60a8aaba19
Idan Yehiel <idan.yehiel@post.runi.ac.il>
2022-11-17 10:16:01 +0200

    DS-04

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -1,12 +1,34 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-//
-// This program only needs to handle arguments that satisfy
-// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
-
-// Put your code here.
+@i
+M = 0
+@R2
+M = 0
+(LOOP)
+     @R1 //checks if R1==i
+     D = M
+     @i
+     D = D-M // R1 = R1-i
+     @12
+     D;JEQ
+    @LOOP_END
+    D;JEQ
+     @R0 //if R0==0 or R1==0
+     D = M
+     @R2
+     M = D
+     @LOOP_END
+     D;JEQ
+     @R1
+     D = M
+     @R2
+     M = D
+     @LOOP_END
+     D;JEQ
+     @R0 
+     D = M
+     @R2 // R2=R2+R0
+     M = M + D
+    @LOOP
+    0;JMP
+(LOOP_END)
+@33
+0;JMP
