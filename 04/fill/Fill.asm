54066c0004cadfdfa6b1e23a78a755e5d5d23b9d
OritYs <orityair1997@gmail.com>
2022-11-23 22:07:23 +0200

    issues: DS-04 fixing bug

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -1,54 +1,40 @@
-@i // i = 0
-M = 0
-@KBD // EndScreen = KDB - 1 - screen 
-D = A - 1
-@SCREEN 
-D = D - A 
-@END_SCREEN
-M = D + 1
+(START) // initializing i = screen
+     @SCREEN
+     D = A
+     @i // i = screen
+     M = D
+     @LOOP
+     0;JMP
+
 (LOOP)
-     @END_SCREEN // if (endscreen - i == 0)
+     @i // if (KBD - i == 0)
      D = M
-     @i
-     D = D - M
-    @LOOP_END // if (D == 0)
-    D;JEQ
      @KBD
+     D = A - D
+     @START
+     D;JEQ
+     @KBD // if KBD == 0 > run WHITE, otherwise BLACK
      D = M
      @WHITE
      D;JEQ
      @BLACK
      D;JNE
-    (CONT_LOOP)
+    (CONT_LOOP) // after set the pixel, we will promote i
      @i
-     M = M + 1 // i++
-    @LOOP
-    0;JMP
-(LOOP_END)
+     M = M + 1
+     @LOOP
+     0;JMP
+
 (BLACK)
-     @i 
-     D = M
-     @SCREEN // A = Screen + i
-     D = A + D 
-     @temp
-     M = D
-     @temp
+     @i   
      A = M
      M = -1 // M[A] = -1
      @CONT_LOOP
      0;JMP
+     
 (WHITE)
-     @i 
-     D = M
-     @SCREEN // A = Screen + i
-     A = A + D
-     @temp
-     M = D
-     @temp
+     @i
      A = M
      M = 0 // M[A] = 0
      @CONT_LOOP
-     0;JMP
-(END)
-     @END
      0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,14 +9,16 @@ M = 0
      D = D - M // R1 = R1-i
      @LOOP_END
      D;JEQ
-     @R0 //if R0==0 or R1==0
+     @R0 
      D = M 
-     @R2
+     @R2 // add R0 to the value of R2
      M = M + D
      @i
      M = M + 1
     @LOOP
     0;JMP
 (LOOP_END)
-@21
-0;JMP
+
+(END)
+     @END
+     0;JMP
