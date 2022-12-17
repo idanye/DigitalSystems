f52b314c45baf24cb9a83a7b8e762f04c58aba4b
Idan Yehiel <idan.yehiel@post.runi.ac.il>
2022-12-17 19:47:03 +0200

    Write Push and Pop left to implement

--- 07/vm_translator/code_writer.py
+++ 07/vm_translator/code_writer.py
@@ -9,6 +9,7 @@ class CodeWriter:
         :param output_file: string
         """
         self.__file = open(output_file, "w")
+        self.__label_index = 0
 
     def write_arithmetic(self, command):
         """
@@ -22,10 +23,21 @@ class CodeWriter:
             self.__write_lst_to_file(self.__translate_arithm_to_asm("-", 2))
         if command == str(CommandType.NEG.value).lower():
             self.__write_lst_to_file(self.__translate_arithm_to_asm("-", 1))
+        if command == str(CommandType.NEG.value).lower():
+            self.__write_lst_to_file(self.__translate_arithm_to_asm("!", 1))
         if command == str(CommandType.OR.value).lower():
             self.__write_lst_to_file(self.__translate_arithm_to_asm("|", 2))
+        if command == str(CommandType.NEG.value).lower():
+            self.__write_lst_to_file(self.__translate_arithm_to_asm("&", 2))
         if command == str(CommandType.EQ.value).lower():
-            self.__write_lst_to_file(self.__translate_arithm_to_asm("=", 2))
+            self.__write_lst_to_file(self.__translate_arithm_to_asm("EQ", 2, is_compare_operator=True))
+            self.__label_index += 1
+        if command == str(CommandType.GT.value).lower():
+            self.__write_lst_to_file(self.__translate_arithm_to_asm("GT", 2, is_compare_operator=True))
+            self.__label_index += 1
+        if command == str(CommandType.LT.value).lower():
+            self.__write_lst_to_file(self.__translate_arithm_to_asm("LT", 2, is_compare_operator=True))
+            self.__label_index += 1
 
     def write_push_pop(self, command, segment, index):
         """
@@ -43,18 +55,37 @@ class CodeWriter:
         """
         self.__file.close()
 
-    @staticmethod
-    def __translate_arithm_to_asm(sign, arg):
-        lst = ["@SP", "M=M-1", "A=M", "D=M"]
-        if arg == 2 and not sign == "=":
-            lst.append("@SP")
-            lst.append("M=M-1")
-            lst.append("A=M")
+    def __translate_arithm_to_asm(self, sign, arg, is_compare_operator=False):
+        temp = ""
+        lst = ["@SP", "M=M-1", "A=M"]
+
+        if is_compare_operator:
+            temp = sign
+            sign = "-"
+
+        if arg == 2:
+            lst.append("D=M")
+            lst.append("A=A-1")
             lst.append(f"M=D{sign}M")
+
         elif arg == 1:
             lst.append(f"M={sign}M")
-        elif sign == "=":
-            pass
+
+        elif is_compare_operator:
+            sign = temp
+            lst.append(f"@TRUE{self.__label_index}")
+            lst.append(f"D;J{sign}")
+            lst.append("@SP")
+            lst.append("A=M-1")
+            lst.append("M=0")
+            lst.append(f"@END_TRUE{self.__label_index}")
+            lst.append("0;JMP")
+            lst.append(f"(TRUE{self.__label_index})")
+            lst.append("@SP")
+            lst.append("A=M-1")
+            lst.append("M=-1")
+            lst.append(f"(END_TRUE{self.__label_index})")
+
         lst.append("@SP")
         lst.append("M=M+1")
         return lst
--- 07/vm_translator/main.py
+++ 07/vm_translator/main.py
@@ -1,17 +1,28 @@
 from parser import Parser
+from command_type import CommandType
+from code_writer import CodeWriter
+import sys
+
+
+def main(file_name):
+    parser = Parser(file_name)
+    output_file_name = file_name.replace(".vm", ".asm")
+    code_writer = CodeWriter(output_file_name)
+
+    while parser.has_more_lines():
+        if parser.command_type() == "C_ARITHMETIC":
+            command = parser.arg1()
+            code_writer.write_arithmetic(command)
+        elif parser.command_type() == "C_PUSH" or parser.command_type() == "C_POP":
+            command = CommandType(parser.command_type()).name
+            memory_type = parser.arg1()
+            index = parser.arg2()
+            code_writer.write_push_pop(command, memory_type, index)
+        parser.advance()
+
+    code_writer.close()
 
 
 if __name__ == '__main__':
-    parser = Parser("/Users/ority/Desktop/Studies/Digital systems/nand2tetris/repo/DigitalSystems/07/StackArithmetic/SimpleAdd/SimpleAdd.vm")
-    print(parser.get_current_line())
-    print(parser.command_type())
-    print(f"first argument: {parser.arg1()}")
-    print(f"second argument: {parser.arg2()}")
-
-    parser.advance()
-    parser.advance()
-    print(parser.get_current_line())
-    print(parser.command_type())
-    print(f"first argument: {parser.arg1()}")
-    print(f"second argument: {parser.arg2()}")
-
+    path = sys.argv[1]
+    main(path)
